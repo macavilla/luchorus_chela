bcf93402865359f9c83da22dbb9f8059bca11d25
macavilla <maca.villa.a@gmail.com>
2025-11-10 23:50:27 -0300

    update kaleidoSonoro

--- src/lib/hydra-patches/kaleidoSonoro.js
+++ src/lib/hydra-patches/kaleidoSonoro.js
@@ -1,25 +1,29 @@
 export default function kaleidoSonoro(hydra, { audioLevel, centroid, energy, amplitude }) {
   // Normalizaciones básicas
-  const level = Math.min(audioLevel * 5, 1); // más sensible
-  const freq = 5 + centroid / 50; // frecuencia base modulada por el centroide
-  const rot = Math.sin(centroid / 50) * 0.5; // rotación oscilante por el timbre
-  const scaleFactor = 1 + level * 0.5; // zoom según volumen
-  const poster = Math.floor(2 + amplitude / 40); // posterize según intensidad
-  const scrollX = () => Math.sin(energy / 5000) * 0.3;
-  const scrollY = () => Math.cos(energy / 7000) * 0.3;
+  const level = Math.min(audioLevel * 5, 1); // audioLevel más sensible
+  const freq = 5 + centroid / 50; // frecuencia base modulada x centroide
+  const rot = Math.sin(centroid / 50) * 0.5; // rotación oscilante x centroide
+  const scaleFactor = 1 + level; // zoom según volumen ?
+  const poster = Math.floor(2 + amplitude / 40); // posterize según intensidad ?
+  const scrollX = () => Math.sin(energy / 5000) * 0.1;
+  const scrollY = () => Math.sin(energy / 5000) * 0.1;
 
   hydra.synth
-    .osc(freq, 0.1 + level * 0.2, 0.1 + level * 0.2)
-    .rotate(rot, 0.1)
+    .osc(freq)
+    .rotate(rot)
     .posterize(poster)
-    .kaleid(6 + Math.round(amplitude / 20))
-    .scroll(scrollX, scrollY, 0.1, 0.1)
+    .kaleid(1 + Math.round(amplitude / 20))    
+    
+    .scroll(scrollX, scrollY)
     .modulate(
-      hydra.synth.noise(2 + level * 3).scale(scaleFactor),
+      hydra.synth.noise(level * 5).scale(scaleFactor),
       () => level * 0.3
     )
-    .saturate(1 + level * 2)
-    .scale(scaleFactor)
+    // .repeat(2, 2, .5)
+    // .saturate(1 + level * 2)
+    .scale(scaleFactor,1, ()=>hydra.synth.width/hydra.synth.height)
+    .contrast(-2)
+    // .scale(1, )
     .out();
 }
 
