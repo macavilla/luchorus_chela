e142eec760e32cbc0079b758ba6c3a57c5cab8b6
macavilla <maca.villa.a@gmail.com>
2025-11-12 00:11:01 -0300

    pulido kaleido 4

--- src/lib/hydra-patches/kaleidoSonoro4.js
+++ src/lib/hydra-patches/kaleidoSonoro4.js
@@ -1,12 +1,12 @@
 export default function kaleidoSonoro(hydra, { audioLevel, centroid, energy, amplitude }) {
 const sides = Math.round(2 + Math.min(centroid / 10, 8)); 
-const rotSpeed = ((amplitude / 127) * 360)/100;
-const azul = .25 + audioLevel * 10 ;
+let smoothRot = 0;
+const azul = .5 + audioLevel * 10 ;
+smoothRot += ((amplitude / 127) * 0.2 - smoothRot) * 0.1;
 
-console.log('rotSpeed', rotSpeed)
 hydra.synth.voronoi(sides, .1, .2).color(.1, .1, azul).brightness(.1).contrast(1.3)//.posterize(5)
   .kaleid(sides)
-  .rotate(rotSpeed)
+  .rotate( ()=> hydra.synth.time * smoothRot)
   .pixelate(100, 100)
   
   .scale(.7, 1, ()=> hydra.synth.width/hydra.synth.height)
